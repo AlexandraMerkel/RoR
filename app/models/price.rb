87a66cc74f83bcd69961693ea452431fb8b8b27f
AlexandraMerkel <gidrargirum.games@gmail.com>
2016-04-19 19:58:41 +0400

    added validations

--- app/models/price.rb
+++ app/models/price.rb
@@ -2,6 +2,8 @@ class Price < ActiveRecord::Base
   belongs_to :cinema_timetable
   belongs_to :zone
 
-  validates :name, presence: true, numericality: {only_integer: true, greater_than: 0}
-  validates :cinema_timetable_id, uniqueness: {scope: :zone_id}
+  validates :cost, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validates :cinema_timetable_id, presence: true, uniqueness: {scope: :zone_id}
+  validates :zone_id, presence: true
+  
 end
